ed43c9452d1780819f7fddf1aba0e1c032ff840d
Brian Ford <brianfordcode@gmail.com>
2024-07-23 18:15:38 -0500

    add looping video background

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 
   
-  <navBar/>
-  
+  <!-- <navBar/> -->
+
   <router-view />
 
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
+<div class="video-container">
+    <video class="loop-video" autoplay loop muted src="/cabasapla-drone-1000kbps.mp4"></video>
+</div>
+
 <div class="main-container">
-    <img id="logo" src="/cabasapla-logo-white.png" alt="logo">
+    <div class="logo-container">
+        <img id="logo" src="/cabasapla-logo-white.png" alt="logo">
+    </div>
+    
 </div>
 
 </template>
@@ -35,13 +42,29 @@ import navBar from '../components/nav-bar.vue';
 
 <style scoped>
 
-.main-container {
-}
-
 #logo {
     width: 300px;
     margin-left: 45px;
-    filter: invert(100%); 
+    /* filter: invert(100%);  */
+}
+
+.logo-container {
+    background-color: rgba(0, 0, 0, 0.75);
+    width: max-content;
+    padding-right: 20px;
+    border-bottom-right-radius: 100px;
+}
+
+.loop-video {
+    width: 100%;
+}
+
+.video-container {
+    width: 100vw;
+    overflow: hidden;
+    height: 100%;
+    position: absolute;
+    z-index: -1000;
 }
 
 </style>
