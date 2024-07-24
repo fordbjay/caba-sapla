6fb9e07f1510e36f6378f480fe5c96bf4c682163
Brian Ford <brianfordcode@gmail.com>
2024-07-23 23:22:53 -0500

    add exterior background image and add action button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
-<div class="video-container">
-    <video class="loop-video" autoplay loop muted src="/cabasapla-drone-1000kbps.mp4"></video>
-</div>
+
 
 <div class="main-container">
-    <div class="logo-container">
-        <img id="logo" src="/cabasapla-logo-white.png" alt="logo">
-    </div>
+
+    <img id="logo" src="/cabasapla-logo-white.png" alt="logo">
+    <a class="action-btn" href="https://google.com" target="_blank">Submit Application</a>
+
+    <!-- <h1 class="tag-line-container">Your Next Home in the Riviera Maya</h1>   -->
+    
+
     
 </div>
+
+<video class="loop-video" autoplay loop muted src="/cabasapla-drone-1000kbps.mp4"></video>
 
 </template>
 
@@ -42,29 +46,52 @@ import navBar from '../components/nav-bar.vue';
 
 <style scoped>
 
-#logo {
-    width: 300px;
-    margin-left: 45px;
-    /* filter: invert(100%);  */
+.main-container {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items:flex-start;
+    height: max-content;
+    background-image: url(/public/exterior.jpg);
+    height: 750px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    /* position: relative; */
 }
 
-.logo-container {
-    background-color: rgba(0, 0, 0, 0.75);
-    width: max-content;
-    padding-right: 20px;
-    border-bottom-right-radius: 100px;
+#logo {
+    width: 40%;
+    max-width: 300px;
+    margin-left: 45px;
+}
+
+.action-btn {
+    cursor: pointer;
+    text-decoration: none;
+    margin: 60px;
+    padding: 20px 40px;
+    border-radius: 50px;
+    border: none;
+    background-color: green;
+    color: white;
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
 }
 
 .loop-video {
     width: 100%;
-}
-
-.video-container {
-    width: 100vw;
-    overflow: hidden;
-    height: 100%;
     position: absolute;
     z-index: -1000;
 }
+
+/* .tag-line-container {
+    color: white;
+    font-size: 50px;
+    font-family: cursive;
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+    border:1px solid;
+} */
 
 </style>
