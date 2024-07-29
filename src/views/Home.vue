3b857887cafb045f5f81df1c771a911ef640b4b4
Brian Ford <brianfordcode@gmail.com>
2024-07-29 17:42:20 -0500

    reorder and edit map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,19 @@
 
     <units/>
 
+    <iframe 
+      src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2426.900296943683!2d-87.0757832430915!3d20.63265993607675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1721952726676!5m2!1sen!2sus"
+      width="100%"
+      height="450"
+      style="border:none;"
+      allowfullscreen="" 
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    >
+    </iframe>
+
     <drone/>
 
-    <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2426.900296943683!2d-87.0757832430915!3d20.63265993607675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1721952726676!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
 
 
 </template>
